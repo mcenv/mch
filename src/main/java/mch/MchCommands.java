b3c584c94f4581e20d5187a4e148c325dc92584d
intsuc <intsuc@gmail.com>
2022-08-02 05:43:44 +0900

    Add command injector

---
+++ src/main/java/mch/MchCommands.java
@@ -0,0 +1,9 @@
+package mch;
+
+import com.mojang.brigadier.CommandDispatcher;
+
+public class MchCommands {
+    public static void register(CommandDispatcher<Object> dispatcher) {
+        // TODO
+    }
+}
--- src/main/java/mch/Transformer.java
+++ src/main/java/mch/Transformer.java
@@ -9,7 +9,7 @@ import java.lang.instrument.ClassFileTransformer;
 import java.security.ProtectionDomain;
 import java.util.function.Function;
 
-import static org.objectweb.asm.Opcodes.ASM9;
+import static org.objectweb.asm.Opcodes.*;
 
 public class Transformer implements ClassFileTransformer {
     private static byte[] transform(byte[] classfileBuffer, Function<ClassVisitor, ClassVisitor> createClassVisitor) {
@@ -24,6 +24,7 @@ public class Transformer implements ClassFileTransformer {
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
         return switch (className) {
             case "net/minecraft/server/MinecraftServer" -> transform(classfileBuffer, ModNameTransformer::new);
+            case "com/mojang/brigadier/CommandDispatcher" -> transform(classfileBuffer, CommandInjector::new);
             default -> null;
         };
     }
@@ -41,6 +42,31 @@ public class Transformer implements ClassFileTransformer {
                     @Override
                     public void visitLdcInsn(Object value) {
                         super.visitLdcInsn("mch");
+                    }
+                };
+            } else {
+                return parent;
+            }
+        }
+    }
+
+    static class CommandInjector extends ClassVisitor {
+        public CommandInjector(ClassVisitor classVisitor) {
+            super(ASM9, classVisitor);
+        }
+
+        @Override
+        public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+            var parent = super.visitMethod(access, name, descriptor, signature, exceptions);
+            if (name.equals("<init>") && descriptor.equals("(Lcom/mojang/brigadier/tree/RootCommandNode;)V")) {
+                return new MethodVisitor(ASM9, parent) {
+                    @Override
+                    public void visitInsn(int opcode) {
+                        if (opcode == RETURN) {
+                            visitVarInsn(ALOAD, 0);
+                            visitMethodInsn(INVOKESTATIC, "mch/MchCommands", "register", "(Lcom/mojang/brigadier/CommandDispatcher;)V", false);
+                        }
+                        super.visitInsn(opcode);
                     }
                 };
             } else {
