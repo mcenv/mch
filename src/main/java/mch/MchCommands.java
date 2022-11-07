c2f34d51f5775a4f7f16cdfd57e6da5e1e018534
intsuc <intsuc@gmail.com>
2022-11-08 02:21:26 +0900

    Use different local config for dry and iteration runs

--- src/main/java/mch/MchCommands.java
+++ src/main/java/mch/MchCommands.java
@@ -11,8 +11,8 @@ import java.util.concurrent.TimeUnit;
 import static com.mojang.brigadier.builder.LiteralArgumentBuilder.literal;
 import static mch.Util.doubleToBytes;
 
-@Keep
 @SuppressWarnings("unused")
+@Keep
 public final class MchCommands {
   private static long startTime;
   private static int iterationCount;
@@ -28,83 +28,96 @@ public final class MchCommands {
     final CommandDispatcher<Object> dispatcher,
     final LocalConfig config
   ) throws IOException {
-    if (config.fork() == LocalConfig.DRY_RUN_FORK) {
-      dispatcher.register(
-        literal("mch:start").executes(c -> {
-          System.out.println("Dry run");
-          return 0;
-        })
-      );
+    if (config instanceof LocalConfig.Dry) {
+      registerDry(dispatcher);
+    } else if (config instanceof LocalConfig.Iteration iteration) {
+      registerIteration(dispatcher, iteration);
+    }
+  }
 
-      dispatcher.register(
-        literal("mch:loop").executes(c -> 0)
-      );
-    } else {
-      final var socket = new Socket((String) null, config.port());
-      final int warmupCount = config.warmupIterations();
-      final int measurementCount = config.warmupIterations() + config.measurementIterations();
-      final long time = TimeUnit.SECONDS.toNanos(config.time());
+  private static void registerDry(
+    final CommandDispatcher<Object> dispatcher
+  ) {
+    dispatcher.register(
+      literal("mch:start").executes(c -> {
+        System.out.println("Dry run");
+        return 0;
+      })
+    );
 
-      dispatcher.register(
-        literal("mch:start").executes(c -> {
-          System.out.println(config.benchmark() + " " + (config.fork() + 1) + "/" + config.forks());
+    dispatcher.register(
+      literal("mch:loop").executes(c -> 0)
+    );
+  }
 
-          final var source = c.getSource();
-          run = dispatcher.parse("function " + config.benchmark(), source);
-          loop = dispatcher.parse("function mch:loop", source);
+  private static void registerIteration(
+    final CommandDispatcher<Object> dispatcher,
+    final LocalConfig.Iteration config
+  ) throws IOException {
+    final var socket = new Socket((String) null, config.port());
+    final int warmupCount = config.warmupIterations();
+    final int measurementCount = config.warmupIterations() + config.measurementIterations();
+    final long time = TimeUnit.SECONDS.toNanos(config.time());
 
-          startTime = System.nanoTime();
+    dispatcher.register(
+      literal("mch:start").executes(c -> {
+        System.out.println(config.benchmark() + " " + (config.fork() + 1) + "/" + config.forks());
 
+        final var source = c.getSource();
+        run = dispatcher.parse("function " + config.benchmark(), source);
+        loop = dispatcher.parse("function mch:loop", source);
+
+        startTime = System.nanoTime();
+
+        try {
+          dispatcher.execute(run);
+        } catch (final CommandSyntaxException e1) {
+          System.out.println(e1.getMessage());
           try {
-            dispatcher.execute(run);
-          } catch (final CommandSyntaxException e1) {
-            System.out.println(e1.getMessage());
-            try {
-              socket.close();
-              dispatcher.execute("function mch:post", source);
-            } catch (IOException e2) {
-              throw new RuntimeException(e2);
-            }
+            socket.close();
+            dispatcher.execute("function mch:post", source);
+          } catch (IOException e2) {
+            throw new RuntimeException(e2);
           }
+        }
+        dispatcher.execute(loop);
+        ++operationCount;
+
+        return 0;
+      })
+    );
+
+    dispatcher.register(
+      literal("mch:loop").executes(c -> {
+        try {
+          final var stopTime = System.nanoTime();
+          if (stopTime - startTime >= time) {
+            final var result = (double) (stopTime - startTime) / (double) operationCount;
+
+            if (iterationCount < warmupCount) {
+              System.out.println("Warmup iteration: " + result + " ns/op");
+            } else if (iterationCount < measurementCount) {
+              System.out.println("Measurement iteration: " + result + " ns/op");
+              socket.getOutputStream().write(doubleToBytes(result));
+            } else {
+              socket.close();
+              return 0;
+            }
+
+            ++iterationCount;
+            operationCount = 0;
+            startTime = System.nanoTime();
+          }
+
+          dispatcher.execute(run);
           dispatcher.execute(loop);
           ++operationCount;
 
           return 0;
-        })
-      );
-
-      dispatcher.register(
-        literal("mch:loop").executes(c -> {
-          try {
-            final var stopTime = System.nanoTime();
-            if (stopTime - startTime >= time) {
-              final var result = (double) (stopTime - startTime) / (double) operationCount;
-
-              if (iterationCount < warmupCount) {
-                System.out.println("Warmup iteration: " + result + " ns/op");
-              } else if (iterationCount < measurementCount) {
-                System.out.println("Measurement iteration: " + result + " ns/op");
-                socket.getOutputStream().write(doubleToBytes(result));
-              } else {
-                socket.close();
-                return 0;
-              }
-
-              ++iterationCount;
-              operationCount = 0;
-              startTime = System.nanoTime();
-            }
-
-            dispatcher.execute(run);
-            dispatcher.execute(loop);
-            ++operationCount;
-
-            return 0;
-          } catch (final IOException e) {
-            throw new RuntimeException(e);
-          }
-        })
-      );
-    }
+        } catch (final IOException e) {
+          throw new RuntimeException(e);
+        }
+      })
+    );
   }
 }
