a0a7ba86d803dd048dd0b48ce73c10ffa815be53
intsuc <intsuc@gmail.com>
2023-02-17 13:23:38 +0900

    Add output time unit configuration
    Closes #14.

--- src/main/java/mch/Util.java
+++ src/main/java/mch/Util.java
@@ -1,5 +1,7 @@
 package mch;
 
+import java.util.concurrent.TimeUnit;
+
 public final class Util {
   public static String quote(
     final String string
@@ -46,5 +48,39 @@ public final class Util {
         ((bytes[6] & 255) << 8) +
         (bytes[7] & 255)
     );
+  }
+
+  public static TimeUnit parseTimeUnit(final String string) {
+    return switch (string) {
+      case "ns" -> TimeUnit.NANOSECONDS;
+      case "us" -> TimeUnit.MICROSECONDS;
+      case "ms" -> TimeUnit.MILLISECONDS;
+      case "s" -> TimeUnit.SECONDS;
+      case "m" -> TimeUnit.MINUTES;
+      default -> throw new IllegalArgumentException(String.format("Invalid time unit: '%s'", string));
+    };
+  }
+
+  public static String abbreviate(final TimeUnit unit) {
+    return switch (unit) {
+      case NANOSECONDS -> "ns";
+      case MICROSECONDS -> "us";
+      case MILLISECONDS -> "ms";
+      case SECONDS -> "s";
+      case MINUTES -> "m";
+      default -> throw new IllegalArgumentException(String.format("Invalid time unit: '%s'", unit));
+    };
+  }
+
+  public static double convert(
+    final double sourceDuration,
+    final TimeUnit sourceUnit,
+    final TimeUnit targetUnit
+  ) {
+    if (sourceUnit.ordinal() < targetUnit.ordinal()) {
+      return sourceDuration / sourceUnit.convert(1, targetUnit);
+    } else {
+      return sourceDuration * targetUnit.convert(1, sourceUnit);
+    }
   }
 }
