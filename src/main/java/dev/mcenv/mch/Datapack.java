62106c491c251e89594fbb8a90854a0572a465b8
intsuc <intsuc@gmail.com>
2023-06-07 20:47:37 +0900

    Generate data pack only when necessary
    Closes #61.

--- src/main/java/dev/mcenv/mch/Datapack.java
+++ src/main/java/dev/mcenv/mch/Datapack.java
@@ -3,16 +3,19 @@ package dev.mcenv.mch;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.GsonBuilder;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 public final class Datapack {
   public static String install(
@@ -20,16 +23,41 @@ public final class Datapack {
     final ServerProperties serverProperties
   ) throws IOException {
     final var serverPath = Paths.get(mchConfig.mc());
-    if (!Files.exists(serverPath) || !Files.isRegularFile(serverPath)) {
+    if (!Files.isRegularFile(serverPath)) {
       throw new IllegalStateException("No server.jar was found");
     }
 
     final var datapack = Paths.get(serverProperties.levelName(), "datapacks", "mch.zip");
     Files.createDirectories(datapack.getParent());
 
-    try (final var out = new BufferedOutputStream(Files.newOutputStream(datapack))) {
-      try (final var in = Main.class.getResourceAsStream("/mch.zip")) {
-        Objects.requireNonNull(in).transferTo(out);
+    final byte[] actualHash;
+    if (Files.isRegularFile(datapack)) {
+      final MessageDigest digest;
+      try {
+        digest = MessageDigest.getInstance("SHA-256");
+      } catch (NoSuchAlgorithmException e) {
+        throw new RuntimeException(e);
+      }
+      try (final var in = new DigestInputStream(new BufferedInputStream(Files.newInputStream(datapack)), digest)) {
+        in.readAllBytes();
+      }
+      actualHash = digest.digest();
+    } else {
+      actualHash = new byte[0];
+    }
+
+    final byte[] expectedHash;
+    try (final var in = Main.class.getResourceAsStream("/hash")) {
+      expectedHash = Objects.requireNonNull(in).readAllBytes();
+    }
+
+    if (!Arrays.equals(actualHash, expectedHash)) {
+      System.out.printf("Installing datapack in %s\n", datapack);
+
+      try (final var out = new BufferedOutputStream(Files.newOutputStream(datapack))) {
+        try (final var in = Main.class.getResourceAsStream("/mch.zip")) {
+          Objects.requireNonNull(in).transferTo(out);
+        }
       }
     }
 
@@ -39,7 +67,6 @@ public final class Datapack {
         .create();
       final var version = gson.fromJson(new String(server.getInputStream(new JarEntry("version.json")).readAllBytes(), StandardCharsets.UTF_8), Version.class);
 
-      System.out.printf("Installing datapack in %s\n", datapack);
       return version.id;
     }
   }
